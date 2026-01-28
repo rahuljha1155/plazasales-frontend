8a7a05b787663764bb590ff9c4b4f7d9b107e0c2
sijanxt <sijanbartaula39@gmail.com>
2026-01-28 13:57:43 +0545

    fix:

--- src/components/home/scroll-hero.tsx
+++ src/components/home/scroll-hero.tsx
@@ -2,22 +2,19 @@
 import { Icon } from "@iconify/react";
 import { TransitionLink } from "../shared";
 import { Button } from "../ui/button";
-import Image from "next/image";
 
 export default function PlazaHero() {
   return (
-    <main className=" min-h-full  lg:min-h-[65vh] py-16 lg:py-0 px-4 xl:px-0 grid lg:grid-cols-2 gap-12 lg:gap-16 max-w-7xl relative mx-auto overflow-hidden">
-      <div className="lg:hidden absolute  inset-0 w-full h-full z-10 pointer-events-none">
-        <Image
+    <main className="min-h-full lg:min-h-[65vh] py-16 lg:py-0 px-4 xl:px-0 grid lg:grid-cols-2 gap-12 lg:gap-16 max-w-7xl relative mx-auto overflow-hidden">
+      <div className="lg:hidden absolute inset-0 w-full h-full z-10 pointer-events-none">
+        <img
           src="/home/globe.png"
           alt="globe image"
-          fill
-          sizes="(max-width: 1024px) 90vw, 50vw"
-          className="translate-y-1/2 object-contain animate-float select-none pointer-events-none"
+          className="size-full translate-y-1/2 object-contain animate-float select-none pointer-events-none"
         />
       </div>
 
-      <div className="space-y-2  sm:space-y-8 relative z-40 flex flex-col items-center text-center lg:text-left lg:items-start justify-center">
+      <div className="space-y-2 sm:space-y-8 relative z-40 flex flex-col items-center text-center lg:text-left lg:items-start justify-center">
         <div className="text-sm sm:text-base lg:text-lg text-center lg:text-left">
           From <span className="text-primary font-medium">core</span> to{" "}
           <span className="text-primary font-medium">edge</span> to{" "}
@@ -63,13 +60,11 @@ export default function PlazaHero() {
           </Button>
         </TransitionLink>
       </div>
-      <div className="hidden lg:flex justify-center items-center w-full relative h-[500px]">
-        <Image
+      <div className="hidden lg:flex justify-center items-center w-full relative">
+        <img
           src="/home/globe.png"
           alt="globe image"
-          fill
-          sizes="50vw"
-          className="object-contain animate-float select-none pointer-events-none"
+          className="size-[75%] xl:size-[80%] 2xl:size-[85%] max-w-[520px] object-contain animate-float select-none pointer-events-none"
         />
       </div>
     </main>
